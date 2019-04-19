9a974fd1f1950af5b3e2a1dff0d7b854e0933701
NeTolya <sloupockpe4enka@gmail.com>
2019-04-19 19:16:25 +0700

    Create server worker #1

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'serverWorker';
+const timeout = 400;
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+];
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+    caches.open(CACHE)
+    .then(function(cache) {
+    	console.log('Установка хэша');
+        return cache.addAll(urlsToCache);
+    }))
+
+});
+
+self.addEventListener('activate', (event) => {
+	 event.waitUntil(self.clients.claim());
+    console.log('Активирован');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Происходит запрос на сервер');
+});
