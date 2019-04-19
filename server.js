eeeef9f36f7fa37251e0dee38faa47efa5ef036b
MacOSO <auroragamesproject@gmail.com>
2019-04-19 16:47:30 +0700

    React starter pack

---
+++ server.js
@@ -0,0 +1,15 @@
+const root = process.cwd(),
+   path = require('path'),
+   express = require('express'),
+   fs = require('fs'),
+   app = express(),
+   resourcesPath = path.join('', '.');
+
+const global = (function() {
+   return this || (0, eval)('this');
+})();
+
+app.use(express.static(resourcesPath));
+const port = process.env.PORT || 777;
+var expressServer = app.listen(port);
+
