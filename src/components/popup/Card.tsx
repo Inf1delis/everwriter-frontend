cca8075ce2f5241da2a2f3ec465e23e99cf64db8
Inf1delis <v.mamedov@g.nsu.ru>
2019-04-20 12:55:39 +0700

    Open&Close PopUp

--- src/components/popup/Card.tsx
+++ src/components/popup/Card.tsx
@@ -1,49 +1,47 @@
 import * as React from "react";
-import {Record } from './../../types';
+import {Record} from './../../types';
 import store from "../../ReduxStore";
 import {addRecord, updateRecord} from "../../actions";
 import {any, string} from "prop-types";
 
 
-class Card extends React.Component<{record:Record, handleClick:(event: any)=>void}, {currentRecord:Record, isNew:string}> {
+class Card extends React.Component<{ record: Record, handleClick: (event: any) => void }, { currentRecord: Record, isNew: string }> {
 
-    constructor(props:{
-        record:Record,
-        handleClick:(event: any)=>void
+    constructor(props: {
+        record: Record,
+        handleClick: (event: any) => void
     }) {
         super(props);
         this.handleSaveOrUpdate = this.handleSaveOrUpdate.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.state = {
-            currentRecord : Object.assign({}, props.record),
-            isNew : props.record.id ? "NO":"YES"
+            currentRecord: Object.assign({}, props.record),
+            isNew: props.record.id ? "NO" : "YES"
         }
     }
 
-    handleChange(event:any){
+    handleChange(event: any) {
         this.state.currentRecord.title = event.target.value;
         console.log(this.state.currentRecord);
     }
 
-    handleSaveOrUpdate(event:any){
+    handleSaveOrUpdate(event: any) {
         store.dispatch(updateRecord(this.state.currentRecord));
         this.props.handleClick(event);
     }
 
     render() {
         return (
-            <div className='card' >
-            <input
-                onChange={this.handleChange}
-                defaultValue={this.state.currentRecord.title}
-            >
-            {/*<input>*/}
-            {/*    <button onClick={this.props.handleClick}>*/}
-            {/*        CLOSE THIS SHIT*/}
-            {/*    </button>*/}
-            {/*</input>*/}
+            <div className='card'>
+                <button onClick={this.props.handleClick}>
+                    CLOSE THIS SHIT
+                </button>
 
-            </input>
+                <input
+                    onChange={this.handleChange}
+                    defaultValue={this.state.currentRecord.title}
+                />
+
                 <button onClick={this.handleSaveOrUpdate}>
                     SAVE
                 </button>
